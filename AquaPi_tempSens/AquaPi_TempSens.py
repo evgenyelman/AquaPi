5488cedb3e75448d94221f98b9d026b192b2e0a1
evgenyelman <evgeny.elman85@gmail.com>
2023-04-29 19:32:14 +0300

    Update AquaPi_TempSens.py
    Added Debug mode, and plotting the temperature

--- AquaPi_tempSens/AquaPi_TempSens.py
+++ AquaPi_tempSens/AquaPi_TempSens.py
@@ -17,23 +17,37 @@ def read_temp_raw():
     f.close()
     return lines
 
-def read_temp():
-    lines = read_temp_raw()
-    while lines[0].strip()[-3:] != 'YES':
-        time.sleep(0.2)
+def read_temp(isDebug):
+    if isDebug:
+        temp_c = np.random.randint(20,30,1)
+        return temp_c        
+    else:
         lines = read_temp_raw()
-    equals_pos = lines[1].find('t=')
-    if equals_pos != -1:
-        temp_string = lines[1][equals_pos+2:]
-        temp_c = float(temp_string) / 1000.0
-        temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            temp_f = temp_c * 9.0 / 5.0 + 32.0
+            return temp_c        
+    
 
 
+# Code starts here
+isDebug = True
 tempVec = np.empty(10)
 while True:
     tempVec = np.roll(tempVec,-1)
-    tempVec[-1] = read_temp()
-    
+    tempVec[-1] = read_temp(isDebug)
+
+    plt.clf() # Clears the previus plot
+    plt.plot(tempVec)
+    plt.ylabel('some numbers')
+    plt.show(block=False) # if block=True script will stop at this line until the figure closed
+    plt.pause(0.05)
+
     print(tempVec)
     time.sleep(1)
+
